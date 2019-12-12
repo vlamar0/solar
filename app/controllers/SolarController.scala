871f59a9b0a25ead32b717e6b6528abe78fcfe84
Vladislav Marinichev <vladislav-marinichev@rambler.ru>
2019-12-12 12:27:05 +0300

    added answered statistic by tag

--- app/controllers/SolarController.scala
+++ app/controllers/SolarController.scala
@@ -14,14 +14,46 @@ class SolarController @Inject()(
   stackOverFlowService: StackOverFlowService
 )(implicit ec: ExecutionContext) extends InjectedController {
 
+  /**
+   * @api {GET} localhost:9000/search
+   * @apiGroup Solar
+   * @apiName Search
+   * @apiParam   {Array[String]}  tag
+   * @apiSuccess {Array[Object]}  list
+   * @apiSuccess {String}         list.tag
+   * @apiSuccess {Number}         list.tag.total
+   * @apiSuccess {Number}         list.tag.answered
+   * @apiSuccessExample {json} Ответ:
+   *  [
+   *    {
+   *      "clojure": {
+   *          "total": 100,
+   *          "answered": 74
+   *      }
+   *    },
+   *    {
+   *      "scala": {
+   *          "total": 100,
+   *          "answered": 41
+   *      }
+   *    },
+   *        ...
+   *  ]
+   */
+
   def search(tag: List[String]) = Action { _ =>
 
-    val jsonsF = tag.map { lang =>
+    val resultStatisticF = tag.map { lang =>
       OptionT(stackOverFlowService.getInfo(lang)).map { statistic =>
-        Json.toJson(lang -> statistic.items.count(_.isAnswered))
-      }.getOrElse(Json.toJson(lang -> "Bad request"))
+        Json.obj(
+          lang -> Json.obj(
+            "total" -> statistic.items.length,
+            "answered" -> statistic.items.count(_.isAnswered)
+          )
+        )
+      } getOrElse Json.obj(lang -> "Bad request")
     }
 
-    Ok(Json.toJson(jsonsF.map(Await.result(_, Duration.Inf))))
+    Ok(Json.toJson(resultStatisticF.map(Await.result(_, Duration.Inf))))
   }
 }
